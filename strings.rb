c07034828919aa3e87cade352de19c75a5407086
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-06 17:43:40 +0000

    automatically backed up by learn

--- strings.rb
+++ strings.rb
@@ -5,4 +5,4 @@ puts "Sarita".reverse
 puts "sarita".swapcase
 puts"sarita".capitalize
 puts"sarita".length
-puts "I love Kode With Klossy".upcase.length
+puts "I love Kode With Klossy".length.upcase
